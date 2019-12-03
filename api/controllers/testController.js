198fea720de5581b1cd46f447002d33decdeb9a7
Fabricio Cardoso <fabricio.cardoso@cubotecnologia.com.br>
2019-12-03 19:06:20 -0300

    Ajuste

--- api/controllers/testController.js
+++ api/controllers/testController.js
@@ -2,5 +2,19 @@
 
 exports.appTest = function(req, res) {
   console.log('Server On');
-  res.json('Server On', Date.now);
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1; //January is 0!
+
+  var yyyy = today.getFullYear();
+  if (dd < 10) {
+    dd = '0' + dd;
+  }
+  if (mm < 10) {
+    mm = '0' + mm;
+  }
+  var today = dd + '/' + mm + '/' + yyyy;
+
+  let info = `Server ON - ${today}`;
+  res.json(info);
 };
